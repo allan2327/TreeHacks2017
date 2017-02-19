e56da3e56c7a1aa2bbe3112967a8061a6a0ffd8f
robbiejones96 <robbiejones96@gmail.com>
2017-02-18 20:30:22 -0800

    First try at Wit response

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ def printf(val):
     print(val)
 
 def send(request, response):
-    print(response['text'])
+    recipient_id = request['session_id']
+    text = response['text']
+    send_message(recipient_id, text)
 actions = {
     'send': send
 }
@@ -50,9 +52,8 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    resp = client.message(message_text)
 
-                    send_message(sender_id, str(resp))
+                    client.run_actions(session_id=sender_id, message=message_text)
 
 
                 if messaging_event.get("delivery"):  # delivery confirmation
